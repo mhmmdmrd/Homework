cdd11f16237fd4466c5c0934bd436c5d6d5f9a21
mhmmdmrd <muradmhmmdit@gmail.com>
2022-07-31 04:40:48 -0400

    homework

---
+++ src/hw1JavaBasics/MySelf.java
@@ -0,0 +1,14 @@
+package hw1JavaBasics;
+/*
+ Project - starts with UpperCase
+ Package - starts with LowerCase
+ Class - starts with UpperCase
+ */
+public class MySelf {
+  // Main method is entry point of Java.
+	public static void main(String[] args) {
+		System.out.println("My name is M Murad");
+		System.out.println("My address is 773 liberty Ave");
+	}
+
+}
